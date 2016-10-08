691429ca41f5d6e0b37956a752546ebaef7fa378
Lian Wang <wanglian1024@gmail.com>
2016-10-08 12:41:54 -0400

    button without text

--- summernote-ext-print.js
+++ summernote-ext-print.js
@@ -46,7 +46,7 @@
       context.memo('button.print', function () {
         // create button
         var button = ui.button({
-          contents: '<i class="fa fa-print"/> ' + lang.print.print,
+          contents: '<i class="fa fa-print"/>',
           tooltip: lang.print.print,
           click: function () {
             self.$printframe.contents().find('body').html(context.invoke('code'));
